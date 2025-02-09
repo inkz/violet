c7eee292333e666b3d9f0dbf44e136a299c6b3bd
violet-dev <violet.dev.master@gmail.com>
2025-02-09 12:59:41 +0900

    Remove deprecated value reference

--- violet/lib/other/named_color.dart
+++ violet/lib/other/named_color.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 
-Map<int, String> colorValueToNameMap = {
-  Colors.red.value: 'red',
-  Colors.pink.value: 'pink',
-  Colors.purple.value: 'purple',
-  Colors.deepPurple.value: 'deepPurple',
-  Colors.indigo.value: 'indigo',
-  Colors.blue.value: 'blue',
-  Colors.lightBlue.value: 'lightBlue',
-  Colors.cyan.value: 'cyan',
-  Colors.teal.value: 'teal',
-  Colors.green.value: 'green',
-  Colors.lightGreen.value: 'lightGreen',
-  Colors.lime.value: 'lime',
-  Colors.yellow.value: 'yellow',
-  Colors.amber.value: 'amber',
-  Colors.orange.value: 'orange',
-  Colors.deepOrange.value: 'deepOrange',
-  Colors.brown.value: 'brown',
-  Colors.grey.value: 'grey',
-  Colors.blueGrey.value: 'blueGrey',
-  Colors.black.value: 'black',
+Map<Color, String> colorValueToNameMap = {
+  Colors.red: 'red',
+  Colors.pink: 'pink',
+  Colors.purple: 'purple',
+  Colors.deepPurple: 'deepPurple',
+  Colors.indigo: 'indigo',
+  Colors.blue: 'blue',
+  Colors.lightBlue: 'lightBlue',
+  Colors.cyan: 'cyan',
+  Colors.teal: 'teal',
+  Colors.green: 'green',
+  Colors.lightGreen: 'lightGreen',
+  Colors.lime: 'lime',
+  Colors.yellow: 'yellow',
+  Colors.amber: 'amber',
+  Colors.orange: 'orange',
+  Colors.deepOrange: 'deepOrange',
+  Colors.brown: 'brown',
+  Colors.grey: 'grey',
+  Colors.blueGrey: 'blueGrey',
+  Colors.black: 'black',
 };
 
 extension NamedColor on Color {
-  String get name => colorValueToNameMap[value]!;
+  String get name => colorValueToNameMap[this]!;
 }
